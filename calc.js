b58d17b84c8d3f8c5a13d3bf2eb858d6ef7d0d51
ftlui <luisekirapa4@gmail.com>
2024-10-15 22:09:02 +0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,16 @@
+//let operator = prompt("enter an operator: (+, -, /, *)");
+//let num1 = parseFloat(prompt("enter the first number: "));
+//let num2 = parseFloat(prompt("enter the second number: "));
+
+//if(operator === "+"){
+  //  result = num1 + num2;
+//} else if(operator === "-"){
+  //  result = num1 - num2
+//}else if(operator === "/"){
+  //  result = num1 / num2
+//}else if(operator === "*"){
+ //   result = num1 * num2
+//}else result = "invalid operator"
+//console.log(result)
+
+//document.write("the result is: " +result)
